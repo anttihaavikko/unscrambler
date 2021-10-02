0b717361f63772b2062b8a735b9c7c4c857ab2de
Antti Haavikko <antti.haavikko@gmail.com>
2021-10-03 00:24:17 +0300

    Decorate start view

--- Assets/Scripts/StartView.cs
+++ Assets/Scripts/StartView.cs
@@ -1,7 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AnttiStarterKit.Extensions;
 using UnityEngine;
 
 public class StartView : MonoBehaviour
 {
+    [SerializeField] private CardHolder holder;
+    [SerializeField] private Elements elements;
+    [SerializeField] private GameObject startCam;
+
+    private void Start()
+    {
+        startCam.SetActive(false);
+        
+        elements.Load();
+        
+        var codes = GetCodes();
+        StartCoroutine(AddLetters(codes));
+    }
+
+    private static List<string> GetCodes()
+    {
+        var sets = new List<List<string>>
+        {
+            new List<string> { "U", "N", "Sc", "Ra", "Nb", "La", "S" },
+            new List<string> { "U", "N", "Sc", "Ra", "Mn", "La", "S" },
+            new List<string> { "U", "N", "Sc", "Ru", "U", "B", "Lu", "Mn" },
+            new List<string> { "U", "N", "Sc", "Ra", "Nb", "La", "Ru" },
+            new List<string> { "U", "N", "Sc", "Ra", "Mn", "La", "Ru" },
+        };
+
+        return sets.Random();
+    }
+
+    private IEnumerator AddLetters(List<string> codes)
+    {
+        foreach (var code in codes)
+        {
+            AddLetter(code);
+            yield return new WaitForSeconds(0.08f);
+        }
+    }
+
+    private void AddLetter(string code)
+    {
+        var card = Instantiate(holder.cardPrefab, Vector3.up * 5f, Quaternion.identity);
+        var e = card.GetComponent<ElementCard>();
+        e.Setup(code);
+        holder.AddCard(card, true);
+    }
+
     public void StarGame()
     {
         SceneChanger.Instance.ChangeScene("Main");
@@ -10,5 +59,13 @@ public class StartView : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Application.isEditor && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneChanger.Instance.ChangeScene("Start");
+        }
     }
 }
