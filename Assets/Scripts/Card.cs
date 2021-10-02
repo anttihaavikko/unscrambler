f6289ce7e0fff805e245b466b4d68820b9f41bc8
Antti Haavikko <antti.haavikko@gmail.com>
2021-10-02 09:21:52 +0300

    Add basic card movement

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AnttiStarterKit.Animations;
+using AnttiStarterKit.Managers;
+using AnttiStarterKit.Visuals;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Card : MonoBehaviour
+{
+
+	[SerializeField] private SortingGroup sortingGroup;
+	[SerializeField] private float moveSpeed = 7f;
+
+	private bool dragging;
+	private Vector3 dragPoint;
+
+	public CardHolder currentHolder;
+
+	private Vector3 startPoint;
+
+	private float height;
+
+	private Vector3 fromPosition, toPosition;
+	private float moveDuration = -1f;
+
+	private float currentSpeed;
+
+	public SpriteRenderer shadow;
+
+	private Vector3 shadowScale;
+
+	private void Start ()
+	{
+		shadowScale = shadow.transform.localScale;
+		currentSpeed = moveSpeed;
+	}
+
+	private void Update ()
+	{
+
+		var lastPos = transform.position;
+
+		if (dragging)
+		{
+			var mousePos = Input.mousePosition;
+			mousePos.z = -Camera.main.transform.position.z + height;
+			mousePos = Camera.main.ScreenToWorldPoint (mousePos);
+
+			transform.position = new Vector3 (mousePos.x, mousePos.y, height * 0.5f) + dragPoint;
+		}
+		else
+		{
+			transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler (Vector3.zero), 1f);
+		}
+
+		if (moveDuration >= 0f && moveDuration <= 1f) {
+			moveDuration += Time.deltaTime * currentSpeed;
+			transform.position = Vector3.Lerp (fromPosition, toPosition, moveDuration);
+		}
+
+		Tilt (lastPos, transform.position);
+		float offset = dragging ? 0.1f : 0f;
+
+		shadow.transform.position = new Vector3 (transform.position.x, transform.position.y, dragging ? -0.25f : 0f);
+		shadow.transform.localScale = dragging ? shadowScale * 1.1f : shadowScale;
+	}
+
+	private void Tilt(Vector3 prevPos, Vector3 curPos) {
+		float maxAngle = 10f;
+
+		float xdiff = curPos.x - prevPos.x;
+		xdiff = Mathf.Clamp (xdiff * 1000f, -maxAngle, maxAngle);
+
+		float ydiff = curPos.y - prevPos.y;
+		ydiff = Mathf.Clamp (ydiff * 1000f, -maxAngle, maxAngle);
+
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler (new Vector3 (-ydiff, xdiff, 0)), 0.5f);
+	}
+
+	public void OnMouseDown() {
+
+		currentSpeed = moveSpeed + Random.Range(-0.5f, 0.5f);
+		
+		SetHeight (true);
+
+		startPoint = transform.position;
+
+		var point = Camera.main.ScreenPointToRay (Input.mousePosition).GetPoint(-Camera.main.transform.position.z - height);
+		point.z = 0;
+
+		dragPoint = Vector3.zero;
+		
+		currentHolder.RemoveCard (this);
+	}
+
+	public void SetHeight(bool raised)
+	{
+		dragging = raised;
+		height = raised ? -1f : 0f;
+		sortingGroup.sortingOrder = raised ? 2 : 1;
+	}
+
+	public void OnMouseUp()
+	{
+		SetHeight (false);
+		currentHolder.AddCard (this, false);
+	}
+
+	void OnMouseOver() {
+		if (dragging) {
+			currentHolder.PreviewCard (this);
+		}
+	}
+
+	private bool LeftArea(float distance) {
+		return (transform.position - startPoint).magnitude > distance;
+	}
+
+	public void Move(Vector3 pos)
+	{
+		fromPosition = transform.position;
+		toPosition = pos;
+		moveDuration = 0f;
+	}
+
+	public void JustRemove() {
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/CardHolder.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CardHolder : MonoBehaviour
+{
+	[SerializeField] private bool isVertical;
+
+	private List<Card> cards;
+	public int cardMax = 1;
+
+	public int numberOfCards = 1;
+	public Card cardPrefab;
+
+	public CardHolder targetHolder;
+
+	public bool demoSpawn = false;
+
+	private Vector3 Forward => isVertical ? Vector3.up : Vector3.right;
+
+	private void Awake ()
+	{
+		cards = new List<Card> ();
+
+		if (demoSpawn)
+		{
+			SpawnNewHand ();
+		}
+	}
+
+	public void SpawnNewHand() {
+		for (var i = 0; i < numberOfCards; i++)
+		{
+			Invoke (nameof(SpawnCard), i * 0.1f);
+		}
+	}
+
+	public void SpawnCard()
+	{
+		if (cards.Count >= cardMax) return;
+		var c = Instantiate (cardPrefab, transform.position + ((cards.Count + 1) * 0.5f * 1.1f + 5f) * Forward, Quaternion.identity);
+		AddCard (c, true);
+		PositionCards ();
+	}
+	
+	private void Update () {
+		if (Application.isEditor) {
+			if (Input.GetKeyDown (KeyCode.Q)) {
+				SpawnCard ();
+			}
+
+			if (Input.GetKeyDown (KeyCode.W)) {
+				PositionCards ();
+			}
+		}
+	}
+
+	public Card RemoveFirst() {
+		Card c = null;
+		if (cards.Count > 0) {
+			c = cards [0];
+			cards.RemoveAt (0);
+		}
+		PositionCards ();
+		return c;
+	}
+
+	public Card RemoveRandom()
+	{
+		Card c = null;
+		var i = Random.Range (0, cards.Count);
+
+		if (cards.Count > 0)
+		{
+			c = cards [i];
+			cards.RemoveAt (i);
+		}
+		PositionCards ();
+		return c;
+	}
+
+	public void RemoveCard(Card c)
+	{
+		if (!cards.Contains(c)) return;
+		cards.Remove (c);
+		PositionCards ();
+	}
+
+	private float GetAxisFor(Vector3 v)
+	{
+		return isVertical ? v.y : v.x;
+	}
+
+	public void PreviewCard(Card c)
+	{
+		var slot = 0;
+		var axisValue = GetAxisFor(c.transform.position);
+
+		for (var i = 0; i < cards.Count; i++)
+		{
+			if (axisValue > GetAxisFor(cards [i].transform.position))
+			{
+				slot = i + 1;
+			}
+		}
+
+		PositionCards (slot);
+	}
+
+	public void AddCard(Card c, bool toEnd)
+	{
+		if(!cards.Contains(c))
+		{
+
+			if (cards.Count >= cardMax)
+			{
+				var swap = cards [0];
+				cards.RemoveAt (0);
+				swap.currentHolder.targetHolder.AddCard (swap, false);
+			}
+
+			var slot = 0;
+
+			if (toEnd) {
+				
+				cards.Add (c);
+
+			}
+			else
+			{
+				for (var i = 0; i < cards.Count; i++)
+				{
+					if (GetAxisFor(c.transform.position) > GetAxisFor(cards [i].transform.position))
+					{
+						slot = i + 1;
+					}
+				}
+
+				cards.Insert (slot, c);
+			}
+		}
+
+		c.currentHolder = this;
+		PositionCards ();
+	}
+
+	public void PositionCards(int spaceBefore = -1)
+	{
+		var areaSize = (cards.Count - 1) * 0.1f + cards.Sum(t => GetAxisFor(t.transform.localScale));
+
+		var curPos = spaceBefore == -1 ? 0f : -0.525f;
+
+		for (var i = 0; i < cards.Count; i++)
+		{
+			curPos += GetAxisFor(cards [i].transform.localScale) * 0.525f;
+
+			if (i == spaceBefore)
+			{
+				curPos += 0.525f * 2;
+			}
+
+			cards [i].Move(transform.position + (-areaSize * 0.5f + curPos) * Forward + Vector3.back * 0.01f);
+			curPos += GetAxisFor(cards [i].transform.localScale) * 0.5f + 0.1f;
+		}
+	}
+
+	public int CardCount() {
+		return cards.Count;
+	}
+}
