57d92725dbb4e67d5b69d32554710a3a50d7b348
Antti Haavikko <antti.haavikko@gmail.com>
2021-10-03 21:21:20 +0300

    Remove secrerts file

--- Assets/Leaderboards/Secrets.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Secrets
-{
-    public static string redditApiKey = "-rHEdO1MXyDpyA";
-
-    public static long GetVerificationNumber(string name, long score, long secondary)
-    {
-        return (score % 456 + (long)name.Length * secondary * 123) % 79971;
-    }
-}
